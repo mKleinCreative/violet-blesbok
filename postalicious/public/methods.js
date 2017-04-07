df248cabf17b6df66dec55f1cb5666e6a2e454bf
Michael Klein <miketheklein@gmail.com>
2017-04-07 14:07:19 -0700

    Able to print query values and keys

--- postalicious/public/methods.js
+++ postalicious/public/methods.js
@@ -1,12 +1,23 @@
 function fetchRequest() {
   const method = document.getElementById('urlMethod').value
   const host = document.getElementById('basic-url').value
+  const Query_Key1 = document.getElementById('Query_Key1').value
+  const Query_Key2 = document.getElementById('Query_Key2').value
+  const Query_Key3 = document.getElementById('Query_Key3').value
+  const Query_Value1 = document.getElementById('Query_Value1').value
+  const Query_Value2 = document.getElementById('Query_Value2').value
+  const Query_Value3 = document.getElementById('Query_Value3').value
   const responseBox = document.getElementsByClassName('response-box')[0]
-
   const requestBox = document.getElementsByClassName('request-box')[0]
   requestBox.innerText = `
     ${method}
     ${host}
+    ${Query_Key1}
+    ${Query_Value1}
+    ${Query_Key2}
+    ${Query_Value2}
+    ${Query_Key3}
+    ${Query_Value3}
   `
   const headers = new Headers()
   headers.append("Content-Type", "application/json")
@@ -14,11 +25,32 @@ function fetchRequest() {
   const options = {
     method: 'POST',
     headers,
-    body: JSON.stringify({method, host})
+    body: JSON.stringify({
+      method,
+      host,
+      Query_Key1,
+      Query_Value1,
+      Query_Key2,
+      Query_Value2,
+      Query_Key3,
+      Query_Value3
+    })
   }
 
   return fetch( 'http://localhost:3001/construct_request', options )
+
     .then(response => response.json())
     .then(response => responseBox.innerText = response)
     .catch(error => console.error({error}))
 }
+
+$("#resp-button").click(function(event){
+  fetchRequest()
+  event.preventDefault()
+  // if (loc.indexOf("?") === -1){
+  //   loc += "?"
+  // }else{
+  //   loc += "&"
+  // }
+  // location.href = loc + "ts=true"
+})
--- postalicious/server.js
+++ postalicious/server.js
@@ -5,7 +5,6 @@ const path = require('path')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
 const requestPromise = require('request-promise')
-
 const port = process.env.PORT || 3001
 const index = require('./form_routes.js')
 
@@ -15,20 +14,25 @@ server.use(express.static(path.join(__dirname, 'public')))
 server.use('/', index)
 
 server.post('/construct_request', function(request, response) {
-  console.log(request.body);
 
   const options = {
     method: `${request.body.method}`,
     uri: `${request.body.host}`,
-    body: JSON.stringify(request.body.request),
+    qs: {
+      [`${request.body.Query_Key1}`]:`${request.body.Query_Value1}`,
+      [`${request.body.Query_Key2}`]:`${request.body.Query_Value2}`,
+      [`${request.body.Query_Key3}`]:`${request.body.Query_Value3}`
+    },
+    body: JSON.stringify(request.body.response),
   }
 
+  console.log('request.body:', request.body)
   requestPromise(options)
     .then(function(results) {
-      console.log('response:', response)
       response.json(results)
     })
 })
+
 
 server.use(logger("combined"))
 
